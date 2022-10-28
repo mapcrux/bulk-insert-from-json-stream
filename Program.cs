294bda078363a83b0b8cb502eebdfaff8e7862b5
Michael Pelland <michael.pelland@optum.com>
2022-10-28 17:41:16 -0400

    Creating Project

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace ConsoleApp3
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            try
+            {
+                var buffer = new BatchBlock<Rate>(10000);
+                var consumerTask = new ActionBlock<Rate[]>(a =>
+                    JsonParser.Consume(a));
+                buffer.LinkTo(consumerTask);
+                buffer.Completion.ContinueWith(delegate { consumerTask.Complete(); });
+                await JsonParser.Produce(buffer);
+                buffer.Complete();
+
+                consumerTask.Completion.Wait();
+                Console.ReadLine();
+            }
+            catch {
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Rate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public class Rate
+    {
+        public string TIN { get; set; }
+        public string TinType { get; set; }
+        public string BillingCode { get; set; }
+        public string BillingCodeType { get; set; }
+        public int BillingCodeTypeVersion { get; set; }
+        public string NegotiatedType { get; set; }
+        public double? NegotiatedRate { get; set; }
+        public DateTime? ExpirationDate { get; set; }
+        public string BillingClass { get; set; }
+
+    }
+}
