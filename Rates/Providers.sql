11007cca89907176089feca3b10c806aff723c93
Michael Pelland <mapcrux@gmail.com>
2022-12-13 15:49:53 -0500

    Updating database schema for multi providers

--- Rates/Providers.sql
+++ Rates/Providers.sql
@@ -1,6 +1,4 @@
 ﻿CREATE TABLE [dbo].[Providers]
 (
-	[Id] INT NOT NULL PRIMARY KEY,
-	[Tin] NVARCHAR(10) Null,
-	[TinType] NVARCHAR(3) Null,
+	[Id] INT NOT NULL PRIMARY KEY
 )
---
+++ Rates/TIN.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[TIN]
+(
+    [Id] UNIQUEIDENTIFIER NOT NULL primary key,
+	[Tin] NVARCHAR(10) Null,
+	[TinType] NVARCHAR(3) Null,	
+	[ProviderId] INT NOT NULL,
+	constraint [FK_TIN_To_Providers] FOREIGN KEY ([ProviderId]) REFERENCES [Providers]([Id])
+)
